c8fab877e202df5723051cd508c41c456ae22060
gab-braga <gabrielbraga0712@gmail.com>
2025-01-04 12:38:57 -0300

    style: css reseted

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 export default () => {
   return (
     <div className={styles.Header}>
-      <nav>
+      <nav className="container">
         <Link to="/">Home</Link>
         <Link to="/login">Login / Criar</Link>
       </nav>
