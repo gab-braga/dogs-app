1bea3bf14a93c71259f9f41e1c5440a598c2b8cc
gab-braga <gabrielbraga0712@gmail.com>
2025-01-04 12:30:44 -0300

    feat: routes configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <Header />
-      <h1>App</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
       <Footer />
-    </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './Home.module.css';
+
+export default () => {
+  return <div className={styles.Home}>Home</div>;
+};
