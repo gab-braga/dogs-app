259511c8988be774025728e18266ddff77d2b56a
gab-braga <gabrielbraga0712@gmail.com>
2025-01-04 16:33:56 -0300

    style: header styled

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import styles from './Header.module.css';
 import { Link } from 'react-router-dom';
+import Dog from '../../assets/dogs.svg?react';
 
 export default () => {
   return (
-    <div className={styles.Header}>
-      <nav className="container">
-        <Link to="/">Home</Link>
-        <Link to="/login">Login / Criar</Link>
+    <header className={styles.Header}>
+      <nav className={[styles.nav, 'container'].join(' ')}>
+        <Link className={styles.logo} to="/" aria-label="Dogs - Home">
+          <Dog />
+        </Link>
+        <Link className={styles.login} to="/login">
+          Login / Criar
+        </Link>
       </nav>
-    </div>
+    </header>
   );
 };
