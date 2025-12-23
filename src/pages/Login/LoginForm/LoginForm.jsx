85fd712c168bd9b87c4420102c60b855ebb7ac5f
gab-braga <gabrielbraga0712@gmail.com>
2025-12-23 12:42:24 -0300

    feat: login form updated

--- src/pages/Login/LoginForm/LoginForm.jsx
+++ src/pages/Login/LoginForm/LoginForm.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Input from '../../../components/Input/Input';
-import Button from '../../../components/Button/Button';
-import useInput from '../../../hooks/useInput';
-import { useAuth } from '../../../context/AuthContext';
-import Error from '../../../components/Helper/Error/Error';
-import styles from './LoginForm.module.css';
-import stylesBtn from '../../../components/Button/Button.module.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import Input from "../../../components/Input/Input";
+import Button from "../../../components/Button/Button";
+import useInput from "../../../hooks/useInput";
+import { useAuth } from "../../../context/AuthContext";
+import Error from "../../../components/Helper/Error/Error";
+import styles from "./LoginForm.module.css";
+import stylesBtn from "../../../components/Button/Button.module.css";
 
 export default () => {
   const { loadding, error, login } = useAuth();
-  const username = useInput('', { required: true });
-  const password = useInput('', { required: true });
+  const username = useInput("", { required: true });
+  const password = useInput("", { required: true });
 
   async function handleSubmit(event) {
     event.preventDefault();
@@ -28,29 +28,38 @@ export default () => {
     const inputs = [username, password];
     const validation = inputs.reduce(
       (value, input) => value && input.validate(),
-      true,
+      true
     );
     return validation;
   }
 
   return (
     <section className="animeLeft">
-      <h1 className="title">Login</h1>
-      <form onSubmit={handleSubmit} className={styles.form}>
-        <Input name="username" label="Usuário" type="text" {...username} />
-        <Input name="password" label="Senha" type="password" {...password} />
-        {loadding ? (
-          <Button disabled>Carregando...</Button>
-        ) : (
-          <Button>Entrar</Button>
-        )}
-        {<Error error={error} />}
-      </form>
-      <Link className={styles.perdeu} to="/login/perdeu">Perdeu a Senha?</Link>
-      <div className={styles.cadastro}>
-        <h2 className={styles.subtitle}>Cadastre-se</h2>
-        <p>Ainda não possui conta? Cadastre-se no site.</p>
-        <Link className={stylesBtn.button} to="/login/criar">Cadastrar</Link>
+      <div className={styles.form}>
+        <h1 className="title">Login</h1>
+
+        <form onSubmit={handleSubmit}>
+          <Input name="username" label="Usuário" type="text" {...username} />
+          <Input name="password" label="Senha" type="password" {...password} />
+          {loadding ? (
+            <Button disabled>Carregando...</Button>
+          ) : (
+            <Button>Entrar</Button>
+          )}
+          {<Error error={error} />}
+        </form>
+
+        <Link className={styles.perdeu} to="/login/perdeu">
+          Perdeu a Senha?
+        </Link>
+
+        <div className={styles.cadastro}>
+          <h2 className={styles.subtitle}>Cadastre-se</h2>
+          <p>Ainda não possui conta? Cadastre-se no site.</p>
+          <Link className={stylesBtn.button} to="/login/criar">
+            Cadastrar
+          </Link>
+        </div>
       </div>
     </section>
   );
